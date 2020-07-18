e622beb2a27067b4407e4572f7db8e1e43096182
Steven Haid <stevenhaid@gmail.com>
2020-07-18 08:37:55 -0400

    add gtft and tester

--- ctlr.c
+++ ctlr.c
@@ -1,3 +1,5 @@
+// XXX rename BE_PASSIVE to DEFEND
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -31,14 +33,26 @@
 //
 
 //
+// typedefs
+//
+
+typedef struct {
+    char *name;
+    int health;
+} health_score_t;
+
+//
 // variables
 //
+
+health_score_t health_score_tbl[100];
 
 //
 // prototypes
 //
 
 void simulate_conflict(char **player, int *health);
+int compare(const void *a, const void *b);
 
 // -------------------------------------------------------
 
@@ -84,9 +98,26 @@ int main(int argc, char **argv)
         printf("         ---------\n");
         printf("%12s %5d\n", "AVG", sum_health/max_players_list);
         printf("\n");
+
+        health_score_tbl[i].name = players_list[i];
+        health_score_tbl[i].health = sum_health/max_players_list;
+    }
+
+    printf("\n");
+    printf("--------- SUMMARY -------------\n");
+    qsort(health_score_tbl, max_players_list, sizeof(health_score_t), compare);
+    for (i = 0; i < max_players_list; i++) {
+        printf("%12s %5d\n", 
+               health_score_tbl[i].name,
+               health_score_tbl[i].health);
     }
 
     return 0;
+}
+
+int compare(const void *a, const void *b)
+{
+    return ((health_score_t*)a)-> health <  ((health_score_t*)b)->health;
 }
 
 void simulate_conflict(char **player, int *health)
@@ -94,12 +125,12 @@ void simulate_conflict(char **player, int *health)
     proc_hndl_t *h[2];
     char         response[2][100];
     char         prior_response[2][100];
-    int          round, idx;
+    int          day, idx;
 
     #define PLAYERS_ACTIONS_ARE(r0,r1) (strcmp(response[0], (r0)) == 0 && \
                                         strcmp(response[1], (r1)) == 0)
 
-    #define MAX_ROUND 100 //XXX
+    #define MAX_DAY 100 //XXX
 
     memset(health,0,2*sizeof(int));
     memset(response,0,sizeof(response));
@@ -114,7 +145,7 @@ void simulate_conflict(char **player, int *health)
     h[1] = proc_run(player[1], "1", NULL);
 
     // loop xxx explain
-    for (round = 1; round <= MAX_ROUND; round++) {
+    for (day = 1; day <= MAX_DAY; day++) {
         // xxx
         //xxx vheck ret here too
         for (idx = 0; idx <= 1; idx++) {
@@ -154,7 +185,7 @@ void simulate_conflict(char **player, int *health)
             exit(1);
         }
 
-        DEBUG("%8d %12d %12d\n", round, health[0], health[1]);
+        DEBUG("%8d %12d %12d\n", day, health[0], health[1]);
     }
 
     proc_printf(h[0], "TERM\n");
