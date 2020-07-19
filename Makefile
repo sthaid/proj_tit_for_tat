73dbf4c613a280dd399c8e7219ec0bb42d178938
Steven Haid <stevenhaid@gmail.com>
2020-07-19 08:40:07 -0400

    code lucifer in c

--- Makefile
+++ Makefile
@@ -1,13 +1,15 @@
-TARGETS = ctlr
+TARGETS = ctlr lucifer
 
 CC = gcc
 OUTPUT_OPTION=-MMD -MP -o $@
 CFLAGS = -g -O2 \
          -Wall -Wextra -Wno-unused-parameter -Wno-sign-compare -Wno-clobbered 
 
-SRC_TFT = ctlr.c utils.c
+SRC_CTLR    = ctlr.c utils.c
+SRC_LUCIFER = lucifer.c
 
-DEP=$(SRC_TFT:.c=.d)
+DEP=$(SRC_CTLR:.c=.d) \
+    $(SRC_LUCIFER:.c=.d)
 
 #
 # build rules
@@ -15,8 +17,11 @@ DEP=$(SRC_TFT:.c=.d)
 
 all: $(TARGETS)
 
-ctlr: $(SRC_TFT:.c=.o)
-	$(CC) -pthread -lreadline -lm -o $@ $(SRC_TFT:.c=.o)
+ctlr: $(SRC_CTLR:.c=.o)
+	$(CC) -pthread -lreadline -lm -o $@ $(SRC_CTLR:.c=.o)
+
+lucifer: $(SRC_LUCIFER:.c=.o)
+	$(CC) -o $@ $(SRC_LUCIFER:.c=.o)
 
 -include $(DEP)
 
---
+++ lucifer.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+//
+// defines
+//
+
+#define ERROR(fmt, args...) \
+    do { \
+        fprintf(stderr, "%s-%s: ERROR: ", progname, idstr); \
+        fprintf(stderr, fmt, ## args); \
+        exit(1); \
+    } while (0)
+
+#define DEBUG(fmt, args...) \
+    do { \
+        if (debug) { \
+            fprintf(stderr, "%s-%s: DEBUG: ", progname, idstr); \
+            fprintf(stderr, fmt, ## args); \
+        } \
+    } while (0)
+
+//
+// variables
+//
+
+int   debug = 0;   // set to 1 to enable debug prints
+char *progname;
+char *idstr;
+
+//
+// prototypes
+//
+
+static void remove_newline(char *s);
+
+// -----------------  LUCIFER  -----------------------------------------------
+
+int main(int argc, char **argv)
+{
+    char *cmd, *arg, input[100];
+    bool yesterday_opponent_attacked;
+
+    // must use line buffered stdout, 
+    // because stdout is a pipe to the ctlr program
+    setlinebuf(stdout);
+
+    // argc must be 2
+    if (argc != 2) {
+        ERROR("argc=%d, must be 2\n", argc);
+    }
+
+    // get progname and idstr from command line args
+    progname = argv[0];
+    idstr = argv[1];
+    if (strcmp(idstr, "0") && strcmp(idstr, "1")) {
+        ERROR("argv[1] must be either '1' or '2'\n");
+    }
+
+    while (fgets(input, sizeof(input), stdin) != NULL) {
+        // remove trailing newline char, this is not usually needed
+        remove_newline(input);
+        DEBUG("RECEIVED %s\n", input);
+
+        // use strtok to splt input to cmd and arg
+        cmd = strtok(input, " \n");
+        arg = strtok(NULL, " \n");
+
+        // input must have a cmd
+        if (cmd == NULL) {
+            ERROR("blank line\n");
+        }
+
+        // process input cmd
+        if (strcmp(cmd, "YESTERDAY_YOUR_OPPONENT") == 0) {
+            yesterday_opponent_attacked = (strcmp(arg, "ATTACKED") == 0);
+            DEBUG("  yesterday_opponent_attacked = %d\n", yesterday_opponent_attacked);
+        } else if (strcmp(cmd, "WHAT_WILL_YOU_DO_TODAY?") == 0) {
+            printf("ATTACK\n");
+        } else if (strcmp(cmd, "TERM") == 0) {
+            exit(0);
+        } else {
+            ERROR("unexpected cmd '%s'\n", cmd);
+        }
+    }
+}
+
+static void remove_newline(char *s)
+{
+    int len = strlen(s);
+
+    if (len > 0 && s[len-1] == '\n') {
+        s[len-1] = '\0';
+    }
+}
